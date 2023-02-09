514878889375448ed575a76b6bf0fad8781dd174
nataliapovaliy <natalia.bg1125@gmail.com>
2023-02-09 18:29:16 +0200

    index

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import App from './components/App';
 import './index.css';
+import { PersistGate } from 'redux-persist/integration/react';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
         <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
