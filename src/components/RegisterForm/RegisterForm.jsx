988a93a735e217340ae4f2e91063a90d14b4d795
nataliapovaliy <natalia.bg1125@gmail.com>
2023-02-12 02:40:46 +0200

    save

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { registerUser } from '../../services/auth-services/auth-services';
+import { Input, Stack, Button, Heading, Flex, Box, Spacer,ButtonGroup } from '@chakra-ui/react';
 
 export const RegisterForm = () => {
     const [userName, setUserName] = useState('');
@@ -31,20 +32,31 @@ export const RegisterForm = () => {
     }
 
     return (
-        <form   autoComplete="off">
-        <label >
-            Username
-                <input onChange={handleChange} value={userName} type="text" name="name" />
-        </label>
-        <label >
-            Email
-            <input onChange={handleChange} value={email} type="email" name="email" />
-        </label>
-        <label >
-            Password
-                <input onChange={handleChange} value={password} type="password" name="password" />
-        </label>
-        <button type="submit" onClick={handleSubmit}>Register</button>
-        </form>
+        <Stack spacing={3}  autoComplete="off">
+
+            <Input placeholder='Enter the username you want' onChange={handleChange} value={userName} type="text" name="name" />
+            <Input placeholder='Email' onChange={handleChange} value={email} type="email" name="email" />
+            <Input placeholder='Password' onChange={handleChange} value={password} type="password" name="password" />
+
+        {/* <button type="submit" onClick={handleSubmit}>Register</button> */}
+        </Stack>
     );
 }
+
+//  return (
+        //  <form   autoComplete="off">
+        // <label >
+        //     Username
+        //         <input onChange={handleChange} value={userName} type="text" name="name" />
+        // </label>
+//         <label >
+//             Email
+//             <input onChange={handleChange} value={email} type="email" name="email" />
+//         </label>
+//         <label >
+//             Password
+//                 <input onChange={handleChange} value={password} type="password" name="password" />
+//         </label>
+//         <button type="submit" onClick={handleSubmit}>Register</button>
+//         </form>
+//     );
